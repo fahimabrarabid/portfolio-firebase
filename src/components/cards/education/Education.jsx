cf4baa83268747994630a841064b77410f767ad1
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2023-05-16 15:41:04 +0600

    added additional information in education section

--- src/components/cards/education/Education.jsx
+++ src/components/cards/education/Education.jsx
@@ -9,7 +9,12 @@ const Education = (Info) => {
         <div key={index} className="institute">
           <h3>{edu.institute}</h3>
           <h4>{edu.degree}</h4>
-          <p>{edu.duration}</p>
+          <div className="flex justify-between">
+            <h5>{edu.location}</h5>
+            <h5>{edu.duration}</h5>
+          </div>
+          <p>{edu.status}</p>
+          <div className="gpa">CGPA: {edu.cgpa}</div>
         </div>
       ))}
     </div>
