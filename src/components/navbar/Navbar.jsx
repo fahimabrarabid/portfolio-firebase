c309fadde635168c5cd486d690d73e9e966e0c1f
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2023-05-17 03:45:07 +0600

    fixed the navbar freeze on mobile devices fr

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -6,7 +6,7 @@ import ContactsIcon from '../cards/contacts-icon/ContactsIcon'
 
 const Navbar = () => {
   const [open, setOpen] = useState(false)
-  let toggleState = open ? '12' : '-100'
+  let toggleState = open ? 'top-[12%]' : 'top-[-100%]'
 
   function handleClick() {
     setOpen((open) => !open)
@@ -18,7 +18,7 @@ const Navbar = () => {
         <div className="flex items-center mt-4">
           <RiMenu4Line className="text-3xl cursor-pointer md:hidden text-slate-600" onClick={handleClick} size={30} />
         </div>
-        <div className={`top-[${toggleState}%] duration-500 md:static absolute bg-white md:min-h-fit min-h-[60vh] left-0 md:w-auto  w-full flex items-center px-5`}>
+        <div className={`${toggleState} duration-500 md:static absolute bg-white md:min-h-fit min-h-[60vh] left-0 md:w-auto  w-full flex items-center px-5`}>
           <ul className="flex md:flex-row flex-col md:items-center md:gap-7 gap-5">
             <li>
               <a className="hover:text-gray-500 text-gray-700 font-semibold" href="#">
