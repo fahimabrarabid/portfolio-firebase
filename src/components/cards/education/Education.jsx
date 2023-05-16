b5718beba4627d7a5d93167d15a51bb20895ecfd
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2023-05-16 19:39:44 +0600

    added a few logic to handle alignment of date in education item

--- src/components/cards/education/Education.jsx
+++ src/components/cards/education/Education.jsx
@@ -8,11 +8,31 @@ const Education = (Info) => {
       {Info.education.map((edu, index) => (
         <div key={index} className="institute">
           <h3>{edu.institute}</h3>
-          {edu.degree ? <h4>{edu.degree}</h4> : ''}
-          <div className="flex justify-between">
-            {edu.location ? <h5>{edu.location}</h5> : ''}
-            {edu.duration ? <h5>{edu.duration}</h5> : ''}
-          </div>
+          {/* 
+              if location exist then just render 
+              the degree. else render degree and
+              the date with a flex box. if noting 
+              exist then don render anything.. lol
+          */}
+          {!edu.location ? (
+            <div className="flex justify-between">
+              {edu.degree ? <h4>{edu.degree}</h4> : ''}
+              <h5>{edu.duration}</h5>
+            </div>
+          ) : edu.degree ? (
+            <h4>{edu.degree}</h4>
+          ) : (
+            ''
+          )}
+          {/* Render both if location exist */}
+          {edu.location ? (
+            <div className="flex justify-between">
+              <h5>{edu.location}</h5>
+              <h5>{edu.duration}</h5>
+            </div>
+          ) : (
+            ''
+          )}
           {edu.status ? <p>{edu.status}</p> : ''}
           {edu.gpa ? <div className="gpa">{edu.gpa}</div> : ''}
         </div>
