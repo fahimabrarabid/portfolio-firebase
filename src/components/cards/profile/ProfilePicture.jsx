76fe39801e69c18db3e952c524cbb98b8b2ec3fb
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2023-05-17 02:31:34 +0600

    rollback to old profile styles

--- src/components/cards/profile/ProfilePicture.jsx
+++ src/components/cards/profile/ProfilePicture.jsx
@@ -6,12 +6,8 @@ import Contact from '../contacts/Contacts'
 const ProfilePicture = (props) => {
   return (
     <div className="profile-picture-container">
-      <div className="hidden md:block lg:block w-1/4"></div>
       <div className="profile-picture">
         <img className="picture" src={props.picture} alt="profile picture" />
-      </div>
-      <div className="contact-section">
-        <Contact contact={Info.contact} />
       </div>
     </div>
   )
