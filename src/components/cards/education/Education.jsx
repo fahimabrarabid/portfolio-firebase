3cf197cd44d01d954df907cf1271a5c1fb9d1668
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2023-05-16 17:25:10 +0600

    made education info optional

--- src/components/cards/education/Education.jsx
+++ src/components/cards/education/Education.jsx
@@ -8,13 +8,13 @@ const Education = (Info) => {
       {Info.education.map((edu, index) => (
         <div key={index} className="institute">
           <h3>{edu.institute}</h3>
-          <h4>{edu.degree}</h4>
+          {edu.degree ? <h4>{edu.degree}</h4> : ''}
           <div className="flex justify-between">
-            <h5>{edu.location}</h5>
-            <h5>{edu.duration}</h5>
+            {edu.location ? <h5>{edu.location}</h5> : ''}
+            {edu.duration ? <h5>{edu.duration}</h5> : ''}
           </div>
-          <p>{edu.status}</p>
-          <div className="gpa">CGPA: {edu.cgpa}</div>
+          {edu.status ? <p>{edu.status}</p> : ''}
+          {edu.cgpa ? <div className="gpa">CGPA: {edu.cgpa}</div> : ''}
         </div>
       ))}
     </div>
