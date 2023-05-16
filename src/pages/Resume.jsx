f29efbd585480b97dd1e386a71665df2d508d58c
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2023-05-16 22:28:02 +0600

    removed some component to add them later

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -4,14 +4,6 @@ import useDocumentTitle from '../assets/js/useDocumentTitle'
 import Info from '../configs/data'
 import ProfilePicture from '../components/cards/profile/ProfilePicture'
 import Profile from '../components/cards/profile/Profile'
-import Contact from '../components/cards/contacts/Contacts'
-import Education from '../components/cards/education/Education'
-import Skills from '../components/cards/skills/Skills'
-import Projects from '../components/cards/projects/Projects'
-import Experience from '../components/cards/experience/Experience'
-import Achievements from '../components/cards/achievements/Achievements'
-import { Login } from '../components/googleAuth/Login'
-import Calendar from '../components/googleCalendar/Calendar'
 import About from '../components/cards/about/About'
 
 const Resume = () => {
@@ -24,22 +16,6 @@ const Resume = () => {
         <Profile info={Info.profile} />
         <About info={Info.profile} />
       </div>
-      <div className="bottom">
-        <div className="left-side">
-          {/* <Login /> */}
-          <div className="hideWhenBigger">
-            <Contact contact={Info.contact} showTitle="true" />
-          </div>
-          <Education education={Info.education} />
-          {/* <Achievements achievements={Info.achievements} /> */}
-        </div>
-        <div className="right-side">
-          <Skills skills={Info.skills} />
-          {/* <Projects projects={Info.projects} />
-          <Experience experience={Info.experience} /> */}
-        </div>
-      </div>
-      {/* <Calendar id={Info.calendarID } /> */}
     </div>
   )
 }
