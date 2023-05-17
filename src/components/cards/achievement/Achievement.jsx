4574c9ec38a48a677102b924141d8312b33b042a
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2023-05-17 13:07:45 +0600

    redesigned achievement card

--- src/components/cards/achievement/Achievement.jsx
+++ src/components/cards/achievement/Achievement.jsx
@@ -5,20 +5,22 @@ const Achievement = (Info) => {
   return (
     <div className="achievements">
       <h2>Achievements</h2>
-      {Info.achievements.map((achievement, index) => (
-        <div key={index} className="achievement">
-          <div>
-            <h3>{achievement.title}</h3>
-            <p>{achievement.description}</p>
+      <div className="achievement-container">
+        {Info.achievements.map((achievement, index) => (
+          <div key={index} className="achievement">
+            <div>
+              <h3>{achievement.title}</h3>
+              <p>{achievement.description}</p>
+            </div>
+            <div className="achievement-bottom">
+              <h4>{achievement.year}</h4>
+              <a target="_blank" href={achievement.link}>
+                Link
+              </a>
+            </div>
           </div>
-          <div className="achievement-bottom">
-            <h4>{achievement.year}</h4>
-            <a target="_blank" href={achievement.link}>
-              Link
-            </a>
-          </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   )
 }
