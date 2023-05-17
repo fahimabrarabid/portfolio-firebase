37e355d27505d00389eb8ab9ee60fb129c62cce0
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2023-05-17 21:00:06 +0600

    designed appointment page

--- src/pages/appointment/Appointment.jsx
+++ src/pages/appointment/Appointment.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 import './appointment.css'
 import Calendar from '../../components/googleCalendar/Calendar'
 import Info from '../../configs/data'
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
+import { MobileDateTimePicker } from '@mui/x-date-pickers/MobileDateTimePicker'
+import dayjs from 'dayjs'
 
 const Appointment = () => {
+  const [today, setToday] = React.useState(new Date())
+
   return (
     <div className="appointment-container">
       <div className="appointment-header">
@@ -11,6 +17,14 @@ const Appointment = () => {
       </div>
       <div className="appointment-content">
         <Calendar id={Info.calendarID} />
+
+        {/* date picker */}
+        <div className="appointment-request">
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <MobileDateTimePicker orientation="landscape" value={dayjs(today)} onChange={(newValue) => setToday(newValue)} />
+          </LocalizationProvider>
+          <button className="bg-transparent hover:bg-slate-500 text-slate-700 font-semibold hover:text-white py-2 px-4 border border-slate-500 hover:border-transparent rounded">Request an Appointment</button>
+        </div>
       </div>
     </div>
   )
