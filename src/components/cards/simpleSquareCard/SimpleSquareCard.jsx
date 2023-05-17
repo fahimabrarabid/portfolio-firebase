0989e8bc2fe66632b541299a47e6f72e4b9cb27a
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2023-05-17 16:31:11 +0600

    removed auto title from SimpleSquareCard component

--- src/components/cards/simpleSquareCard/SimpleSquareCard.jsx
+++ src/components/cards/simpleSquareCard/SimpleSquareCard.jsx
@@ -4,7 +4,7 @@ import './simnple-square-card.css'
 const SimpleSquareCard = (props) => {
   return (
     <div className="sscs">
-      <h2>{props.title ? props.title : 'No Title'}</h2>
+      {props.title ? <h2>props.title</h2> : ''}
       <div className="ssc-container">
         {props.data.map((d, index) => (
           <div key={index} className="ssc">
