be7741bf54b423e124141d2dceec681119aedfdb
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2023-05-17 10:30:34 +0600

    updated menu names

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -13,14 +13,6 @@ const Navbar = () => {
     setOpen((open) => !open)
   }
 
-  // Home
-  // Academic Background
-  // Professional Experience
-  // Volunteering Experience
-  // Awards and Honors
-  // Research and Publications
-  // Certification and Mentionable Participation
-
   return (
     <header className="w-full">
       <nav className="flex justify-between items-center w-[92%]  mx-auto">
@@ -30,12 +22,11 @@ const Navbar = () => {
         <div className={`${toggleState} duration-500 md:static absolute bg-white md:min-h-fit min-h-[60vh] left-0 md:w-auto  w-full flex items-center px-5`}>
           <ul className="flex md:flex-row flex-col md:items-center md:gap-7 gap-5">
             <NavItem to="/" text="Home" />
-            <NavItem to="/1" text="Academic Background" />
-            <NavItem to="/2" text="Professional Experience" />
-            <NavItem to="/3" text="Volunteering Experience" />
-            <NavItem to="/4" text="Awards and Honors" />
-            <NavItem to="/5" text="Research and Publications" />
-            <NavItem to="/6" text="Certification and Mentionable Participation" />
+            <NavItem to="/1" text="Academic" />
+            <NavItem to="/2" text="Experience" />
+            <NavItem to="/4" text="Achievements" />
+            <NavItem to="/5" text="Research & Publications" />
+            <NavItem to="/6" text="Certification & Participation" />
           </ul>
         </div>
         <div className="flex items-center mt-4">
