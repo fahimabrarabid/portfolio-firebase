213b2f8b093c846766cb3ba2bed6f6d39efff247
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2023-05-17 15:43:49 +0600

    completed Certification page

--- src/pages/certification/Certification.jsx
+++ src/pages/certification/Certification.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import './certification.css'
+import useDocumentTitle from '../../assets/js/useDocumentTitle'
+import Info from '../../configs/data'
+import SimpleSquareCard from '../../components/cards/simpleSquareCard/SimpleSquareCard'
 
 const Certification = () => {
-  return <div>Certification</div>
+  useDocumentTitle('Certification & Participation')
+  return (
+    <div className="certification-container">
+      <SimpleSquareCard data={Info.certification} title="Certification & Participation" />
+    </div>
+  )
 }
 
 export default Certification
