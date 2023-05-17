6b5948c809029c8c7a993fae4c0336ea8ca05267
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2023-05-17 12:51:44 +0600

    added achievements route

---
+++ src/pages/achievements/Achievements.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './achievements.css'
+import Achievement from '../../components/cards/achievement/Achievement'
+import useDocumentTitle from '../../assets/js/useDocumentTitle'
+import Info from '../../configs/data'
+
+const Achievements = () => {
+  return <div>Achievements</div>
+}
+
+export default Achievements
