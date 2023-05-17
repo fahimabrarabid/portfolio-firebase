608c5490d419206f134d9b2679ac7dc73927c7aa
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2023-05-17 11:13:51 +0600

    added new routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Navbar from './components/navbar/Navbar'
+import Education from './pages/education/Education'
 import Home from './pages/home/Home'
 import { Route, Routes, BrowserRouter } from 'react-router-dom'
 
@@ -12,6 +13,7 @@ function App() {
         {/* Routes */}
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="academic" element={<Education />} />
         </Routes>
       </BrowserRouter>
     </>
