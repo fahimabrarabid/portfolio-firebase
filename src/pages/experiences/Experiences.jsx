cff9eb845bef27142f615931d93c8677348efb67
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2023-05-17 11:55:48 +0600

    added 2 section of experiences

--- src/pages/experiences/Experiences.jsx
+++ src/pages/experiences/Experiences.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './experiences.css'
 import Experience from '../../components/cards/experience/Experience'
 import useDocumentTitle from '../../assets/js/useDocumentTitle'
 import Info from '../../configs/data'
@@ -7,8 +8,9 @@ const Experiences = () => {
   useDocumentTitle('Experiences')
 
   return (
-    <div className="container">
-      <Experience experience={Info.experience} />
+    <div className="experiences-container">
+      <Experience experience={Info.experience} title="Professional Experiences" />
+      <Experience experience={Info.experience} title="Volunteering Experiences" />
     </div>
   )
 }
