bd1e17c041c3902aceae2d73958e9365d2b76d83
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2023-05-18 14:24:36 +0600

    change alignment of navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -19,18 +19,20 @@ const Navbar = () => {
   return (
     <header className="w-full">
       <nav className="flex justify-between items-center w-full mx-auto relative z-[1]">
-        <div className="flex items-center mt-4 relative z-10 ml-4">
+        <div className="flex items-center mt-4 relative z-10 ml-4 w-1/12">
           <RiMenu4Line className="text-3xl cursor-pointer md:hidden text-slate-600" onClick={handleClick} size={30} />
         </div>
-        <div className={`${toggleState} bg-white md:bg-slate-700 p-2 mt-2 rounded-lg md:shadow-2xl md:ring-4 md:ring-slate-300 duration-500 md:static absolute md:min-h-fit min-h-[60vh] left-0 md:w-auto  w-full flex items-center px-5`}>
-          <ul className="flex md:flex-row flex-col md:items-center md:gap-7 gap-5">
-            <NavItem onClick={closeMenu} to="/" text="Home" />
-            <NavItem onClick={closeMenu} to="/academic" text="Academic" />
-            <NavItem onClick={closeMenu} to="/experiences" text="Experience" />
-            <NavItem onClick={closeMenu} to="/achievements" text="Achievements" />
-            <NavItem onClick={closeMenu} to="/research" text="Research & Publications" />
-            <NavItem onClick={closeMenu} to="/certification" text="Certification & Participation" />
-          </ul>
+        <div className="w-full flex justify-center">
+          <div className={`${toggleState} bg-white md:bg-slate-700 p-2 mt-2 rounded-lg md:shadow-2xl md:ring-4 md:ring-slate-300 duration-500 md:static absolute md:min-h-fit min-h-[60vh] left-0 md:w-auto  w-full flex items-center px-5`}>
+            <ul className="flex md:flex-row flex-col md:items-center md:gap-7 gap-5">
+              <NavItem onClick={closeMenu} to="/" text="Home" />
+              <NavItem onClick={closeMenu} to="/academic" text="Academic" />
+              <NavItem onClick={closeMenu} to="/experiences" text="Experience" />
+              <NavItem onClick={closeMenu} to="/achievements" text="Achievements" />
+              <NavItem onClick={closeMenu} to="/research" text="Research & Publications" />
+              <NavItem onClick={closeMenu} to="/certification" text="Certification & Participation" />
+            </ul>
+          </div>
         </div>
         <div className="flex items-center mt-4 relative z-10 mr-4">
           <ContactsIcon contact={Info.contact} />
