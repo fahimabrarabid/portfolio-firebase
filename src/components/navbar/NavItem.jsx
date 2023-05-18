3eaf061ff84550f83e38341ec907889aeba4c3ac
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2023-05-18 13:22:31 +0600

    added NavLink Animation

--- src/components/navbar/NavItem.jsx
+++ src/components/navbar/NavItem.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import { motion } from 'framer-motion'
 
 const NavItem = (props) => {
+  const isActiveLink = () => {
+    return props.to === window.location.pathname
+  }
+
   return (
     <li>
-      <NavLink onClick={props.onClick} to={props.to} className={({ isActive }) => (isActive ? 'border-b-4 border-slate-800 md:border-slate-300' : '')}>
+      <NavLink onClick={props.onClick} to={props.to}>
         <span className="hover:text-gray-500 text-gray-700 md:hover:text-gray-400 md:text-gray-100 text-center font-semibold">{props.text}</span>
       </NavLink>
+      {isActiveLink() && <motion.div initial={{ opacity: 0, scale: 0.5 }} animate={{ opacity: 1, scale: 1 }} transition={{ duration: 0.5, type: 'spring', stiffness: 100 }} className="h-1 w-full bg-white"></motion.div>}
     </li>
   )
 }
