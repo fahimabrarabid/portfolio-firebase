1b97d198a6fc0f38a9d54e916b876f7018376a5b
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2023-05-18 23:22:00 +0600

    added total count

--- src/components/cards/about/About.jsx
+++ src/components/cards/about/About.jsx
@@ -15,7 +15,7 @@ const About = (props) => {
     datasets: [
       {
         label: 'Weekly Sales',
-        data: [18, 12, 6, 9, 12, 3, 9],
+        data: [18, 12, 6, 10, 12, 3, 9],
         backgroundColor: ['rgba(255, 26, 104, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(255, 206, 86, 0.2)', 'rgba(75, 192, 192, 0.2)', 'rgba(153, 102, 255, 0.2)', 'rgba(255, 159, 64, 0.2)', 'rgba(0, 0, 0, 0.2)'],
         borderColor: ['rgba(255, 26, 104, 1)', 'rgba(54, 162, 235, 1)', 'rgba(255, 206, 86, 1)', 'rgba(75, 192, 192, 1)', 'rgba(153, 102, 255, 1)', 'rgba(255, 159, 64, 1)', 'rgba(0, 0, 0, 1)'],
         borderWidth: 1,
@@ -27,6 +27,8 @@ const About = (props) => {
     maintainAspectRatio: true,
     // Add any additional chart options here
   }
+
+  const totalCount = data.datasets[0].data.reduce((acc, value) => acc + value, 0)
 
   const [serviceList, setServiceList] = useState([])
   const serviceRef = collection(db, 'services')
@@ -67,8 +69,9 @@ const About = (props) => {
         </div>
       </div>
       <div className="right">
-        <div className="w-80 h-80">
-          <Pie data={data} options={options} />
+        <div className="flex gap-2 content-center items-center">
+          <Pie className="w-80 h-80" data={data} options={options} />
+          <p className="text-center text-lg font-bold mt-4">Total: {totalCount}</p>
         </div>
         <Link className="bg-slate-500 hover:bg-slate-700 text-white font-bold py-2 px-4 rounded-full shadow-lg" to="/appointment">
           Book an Appointment
