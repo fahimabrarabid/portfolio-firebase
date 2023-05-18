3f82f16be3b67ee67d48d1f4b62a0c3be67b1a43
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2023-05-18 16:36:33 +0600

    added initial loading screen

--- src/App.jsx
+++ src/App.jsx
@@ -8,62 +8,79 @@ import Research from './pages/research/Research'
 import Certification from './pages/certification/Certification'
 import Appointment from './pages/appointment/Appointment'
 import { AnimatePresence } from 'framer-motion'
+import Loading from './components/loading/Loading'
+import { useEffect, useState } from 'react'
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    // Simulate an asynchronous operation
+    setTimeout(() => {
+      setIsLoading(false)
+    }, 2000)
+  }, [])
+
   let location = useLocation()
   return (
-    <>
-      {/* NAV */}
-      <Navbar />
+    <div>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <>
+          {/* NAV */}
+          <Navbar />
 
-      {/* Routes */}
-      <AnimatePresence mode="wait">
-        <Routes location={location} key={location.key}>
-          <Route path="/" element={<Home />} />
-          <Route
-            path="academic"
-            element={
-              <div className="w-3/4 mx-auto">
-                <Academics />
-              </div>
-            }
-          />
-          <Route
-            path="experiences"
-            element={
-              <div className="w-3/4 mx-auto">
-                <Experiences />
-              </div>
-            }
-          />
-          <Route
-            path="achievements"
-            element={
-              <div className="w-3/4 mx-auto">
-                <Achievements />
-              </div>
-            }
-          />
-          <Route
-            path="research"
-            element={
-              <div className="w-3/4 mx-auto">
-                <Research />
-              </div>
-            }
-          />
-          <Route
-            path="certification"
-            element={
-              <div className="w-3/4 mx-auto">
-                <Certification />
-              </div>
-            }
-          />
-          <Route path="appointment" element={<Appointment />} />
-        </Routes>
-      </AnimatePresence>
-    </>
+          {/* Routes */}
+          <AnimatePresence mode="wait">
+            <Routes location={location} key={location.key}>
+              <Route path="/" element={<Home />} />
+              <Route
+                path="academic"
+                element={
+                  <div className="w-3/4 mx-auto">
+                    <Academics />
+                  </div>
+                }
+              />
+              <Route
+                path="experiences"
+                element={
+                  <div className="w-3/4 mx-auto">
+                    <Experiences />
+                  </div>
+                }
+              />
+              <Route
+                path="achievements"
+                element={
+                  <div className="w-3/4 mx-auto">
+                    <Achievements />
+                  </div>
+                }
+              />
+              <Route
+                path="research"
+                element={
+                  <div className="w-3/4 mx-auto">
+                    <Research />
+                  </div>
+                }
+              />
+              <Route
+                path="certification"
+                element={
+                  <div className="w-3/4 mx-auto">
+                    <Certification />
+                  </div>
+                }
+              />
+              <Route path="appointment" element={<Appointment />} />
+            </Routes>
+          </AnimatePresence>
+        </>
+      )}
+    </div>
   )
 }
 
---
+++ src/components/loading/Loading.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="flex space-x-4">
+        <div className="w-6 h-6 rounded-full bg-purple-500 animate-pulse-fast"></div>
+        <div className="w-6 h-6 rounded-full bg-blue-500 animate-pulse-fast"></div>
+        <div className="w-6 h-6 rounded-full bg-green-500 animate-pulse-fast"></div>
+      </div>
+    </div>
+  )
+}
+
+export default Loading
