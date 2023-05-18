bad2e6e10b76ae9b48a890552c0b53ff93feda39
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2023-05-18 16:36:22 +0600

    added fast-pulse animation

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,9 @@ module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
+      animation: {
+        'pulse-fast': 'pulse 0.7s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+      },
       screens: {
         xsm: '576px',
         // => @media (min-width: 576px) { ... }
