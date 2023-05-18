331e024b57090dfdb3c12e52a215034ada43dd09
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2023-05-18 23:12:16 +0600

    added dummy pie chart

--- src/components/cards/about/About.jsx
+++ src/components/cards/about/About.jsx
@@ -3,8 +3,31 @@ import { db } from '../../../configs/firebase'
 import { collection, getDocs } from 'firebase/firestore'
 import './about.css'
 import { Link } from 'react-router-dom'
+import Chart, { ArcElement, Tooltip, Legend } from 'chart.js/auto'
+import { Pie } from 'react-chartjs-2'
+
+Chart.register(ArcElement, Tooltip, Legend)
 
 const About = (props) => {
+  // setup
+  const data = {
+    labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+    datasets: [
+      {
+        label: 'Weekly Sales',
+        data: [18, 12, 6, 9, 12, 3, 9],
+        backgroundColor: ['rgba(255, 26, 104, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(255, 206, 86, 0.2)', 'rgba(75, 192, 192, 0.2)', 'rgba(153, 102, 255, 0.2)', 'rgba(255, 159, 64, 0.2)', 'rgba(0, 0, 0, 0.2)'],
+        borderColor: ['rgba(255, 26, 104, 1)', 'rgba(54, 162, 235, 1)', 'rgba(255, 206, 86, 1)', 'rgba(75, 192, 192, 1)', 'rgba(153, 102, 255, 1)', 'rgba(255, 159, 64, 1)', 'rgba(0, 0, 0, 1)'],
+        borderWidth: 1,
+      },
+    ],
+  }
+  const options = {
+    responsive: true,
+    maintainAspectRatio: true,
+    // Add any additional chart options here
+  }
+
   const [serviceList, setServiceList] = useState([])
   const serviceRef = collection(db, 'services')
 
@@ -44,8 +67,12 @@ const About = (props) => {
         </div>
       </div>
       <div className="right">
-        <Link to="/appointment">Book an Appointment</Link>
-        <span>Pie Chart Here</span>
+        <div className="w-80 h-80">
+          <Pie data={data} options={options} />
+        </div>
+        <Link className="bg-slate-500 hover:bg-slate-700 text-white font-bold py-2 px-4 rounded-full shadow-lg" to="/appointment">
+          Book an Appointment
+        </Link>
       </div>
     </div>
   )
