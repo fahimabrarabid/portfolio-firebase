63a2fd0e26cc7c0c4045cc719dec2d15fce748d1
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2023-05-19 02:32:18 +0600

    perfectly working pie chart

--- src/components/cards/about/PieChart.jsx
+++ src/components/cards/about/PieChart.jsx
@@ -8,22 +8,17 @@ Chart.register(ArcElement, Tooltip, Legend)
 
 const PieChart = () => {
   const [counsellingList, setCounsellingList] = useState([])
-  const [pieChartData, setPieChartData] = useState([])
-  const counsellingRef = collection(db, 'counselling')
+  const [pieChartData, setPieChartData] = useState(null)
 
   const getCounsellingList = async () => {
     try {
-      const data = await getDocs(counsellingRef)
+      const data = await getDocs(collection(db, 'counselling'))
       const filteredData = data.docs.map((doc) => ({
         id: doc.id,
         ...doc.data(),
       }))
-      filteredData.filter((data) => data.purpose !== '')
-      setCounsellingList(filteredData)
-      console.log('Counselling List: ')
-      console.log(counsellingList)
+      setCounsellingList(filteredData.filter((data) => data.name !== ''))
     } catch (error) {
-      console.log('error in getCounsellingList: ')
       console.log(error)
     }
   }
@@ -33,6 +28,11 @@ const PieChart = () => {
   }, [])
 
   useEffect(() => {
+    if (counsellingList.length === 0) {
+      setPieChartData(null) // Reset pie chart data if counselling list is empty
+      return
+    }
+
     const countMap = {}
     counsellingList.forEach((counselling) => {
       if (countMap[counselling.purpose]) {
@@ -42,52 +42,43 @@ const PieChart = () => {
       }
     })
 
-    console.log('Count Map: ')
-    console.log(countMap)
+    const purposes = Object.keys(countMap)
+    const backgroundColors = colors.slice(0, purposes.length)
+    const borderColor = colors.slice(0, purposes.length)
 
     const pieChartData = {
-      labels: Object.keys(countMap),
+      labels: purposes,
       datasets: [
         {
           label: 'Counselling Purpose',
-          data: Object.values(countMap),
-          backgroundColor: colors,
-          borderColor: colors,
+          data: purposes.map((purpose) => countMap[purpose]),
+          backgroundColor: backgroundColors,
+          borderColor: borderColor,
           borderWidth: 1,
         },
       ],
     }
 
     setPieChartData(pieChartData)
-    console.log('Pie Chart: ')
-    console.log(pieChartData)
   }, [counsellingList])
 
   const colors = ['#1C2331', '#324556', '#475D6F', '#5E7384', '#748D9A', '#8AA7B1', '#A0C1C8', '#D6E2E9', '#E8F1F4']
-  const data = {
-    labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-    datasets: [
-      {
-        label: 'Weekly Sales',
-        data: [18, 12, 6, 9, 12, 3, 9],
-        backgroundColor: ['rgba(255, 26, 104, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(255, 206, 86, 0.2)', 'rgba(75, 192, 192, 0.2)', 'rgba(153, 102, 255, 0.2)', 'rgba(255, 159, 64, 0.2)', 'rgba(0, 0, 0, 0.2)'],
-        borderColor: ['rgba(255, 26, 104, 1)', 'rgba(54, 162, 235, 1)', 'rgba(255, 206, 86, 1)', 'rgba(75, 192, 192, 1)', 'rgba(153, 102, 255, 1)', 'rgba(255, 159, 64, 1)', 'rgba(0, 0, 0, 1)'],
-        borderWidth: 1,
-      },
-    ],
-  }
+
   const options = {
     responsive: true,
     maintainAspectRatio: true,
     // Add any additional chart options here
   }
 
-  const totalCount = counsellingList.length
+  if (pieChartData === null) {
+    // Render loading state or placeholder when pieChartData is null
+    return <div>Loading...</div>
+  }
 
   return (
     <div className="flex gap-2 content-center items-center">
       <Pie className="w-52 h-52" data={pieChartData} options={options} />
-      <p className="text-center text-lg font-bold mt-4">Total: {totalCount}</p>
+      <p className="text-center text-lg font-bold mt-4">Total: {pieChartData.datasets[0].data.reduce((sum, value) => sum + value, 0)}</p>
     </div>
   )
 }
