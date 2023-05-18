b691e802b591875a8c5b378da308fc0c0d8f3624
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2023-05-19 00:13:09 +0600

    updated the pie chart colors

--- src/components/cards/about/About.jsx
+++ src/components/cards/about/About.jsx
@@ -16,8 +16,8 @@ const About = (props) => {
       {
         label: 'Weekly Sales',
         data: [18, 12, 6, 10, 12, 3, 9],
-        backgroundColor: ['rgba(255, 26, 104, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(255, 206, 86, 0.2)', 'rgba(75, 192, 192, 0.2)', 'rgba(153, 102, 255, 0.2)', 'rgba(255, 159, 64, 0.2)', 'rgba(0, 0, 0, 0.2)'],
-        borderColor: ['rgba(255, 26, 104, 1)', 'rgba(54, 162, 235, 1)', 'rgba(255, 206, 86, 1)', 'rgba(75, 192, 192, 1)', 'rgba(153, 102, 255, 1)', 'rgba(255, 159, 64, 1)', 'rgba(0, 0, 0, 1)'],
+        backgroundColor: ['#1C2331', '#324556', '#475D6F', '#5E7384', '#748D9A', '#8AA7B1', '#A0C1C8', '#D6E2E9', '#ECEFF4'],
+        hoverBackgroundColor: ['#1C2331', '#324556', '#475D6F', '#5E7384', '#748D9A', '#8AA7B1', '#A0C1C8', '#D6E2E9', '#ECEFF4'],
         borderWidth: 1,
       },
     ],
