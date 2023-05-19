6db6ffed8c75cc6c4b7550439f4c36c845998a73
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2023-05-19 07:31:16 +0600

    improved addData function

--- src/configs/addData.js
+++ src/configs/addData.js
@@ -4,8 +4,8 @@ import { collection, addDoc } from 'firebase/firestore'
 const addData = async (tableName, obj) => {
   const tableRef = collection(db, tableName)
   try {
-    const data = await addDoc(tableRef, obj)
-    console.log(`Added ${tableName} data with ID: ${data.id}`)
+    const docRef = await addDoc(tableRef, obj)
+    console.log(`Added ${tableName} data with ID: ${docRef.id}`)
   } catch (error) {
     console.log(`Error adding ${tableName} data:`, error)
   }
