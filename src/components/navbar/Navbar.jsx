55627b6d2dc6a9e9833d9f82ef38543c557b2c97
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2023-05-19 14:53:05 +0600

    made the icon inside only for large devices

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -25,7 +25,7 @@ const Navbar = () => {
         <div className="flex items-center md:hidden mt-4 relative z-10 ml-4 w-1/12">
           <RiMenu4Line className="text-3xl cursor-pointer md:hidden text-slate-600" onClick={handleClick} size={30} />
         </div>
-        <div className="w-full flex justify-center">
+        <div className="w-full flex justify-center md:ml-2 lg:ml-0">
           <div className={`${toggleState} bg-white md:bg-slate-700 p-2 mt-2 rounded-lg md:shadow-2xl md:ring-4 md:ring-slate-300 duration-500 md:static absolute md:min-h-fit min-h-[60vh] left-0 md:w-auto  w-full flex items-center px-5`}>
             <ul className="flex md:flex-row flex-col md:items-center md:gap-7 gap-5">
               <NavItem onClick={closeMenu} to="/" text="Home" />
@@ -36,12 +36,12 @@ const Navbar = () => {
               <NavItem onClick={closeMenu} to="/certification" text="Certification & Participation" />
               {isAdmin && <NavItem onClick={closeMenu} to="/admin" text="Admin Panel" />}
             </ul>
-            <div className="hidden md:flex ml-4">
+            <div className="hidden lg:flex ml-4 md:ml-4">
               <ContactsIcon contact={Info.contact} />
             </div>
           </div>
         </div>
-        <div className="flex md:hidden items-start mt-4 relative z-10 mr-4">
+        <div className="flex lg:hidden items-start mt-4 relative z-10 mr-4">
           <ContactsIcon contact={Info.contact} />
         </div>
       </nav>
