86c213c3d7500fd970b720afee87f02e03aa7bb9
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2023-05-20 20:13:52 +0600

    updated email template

--- src/configs/sendEmail.js
+++ src/configs/sendEmail.js
@@ -1,6 +1,32 @@
 import emailjs from 'emailjs-com'
 
 export const sendEmail = (info) => {
+  const convertDateToURL = (dateString) => {
+    const [datePart, timePart] = dateString.split(' ')
+    const [dd, mm, yyyy] = datePart.split('-')
+    const [hh, min, A] = timePart.split(':')
+
+    const formattedDate = `${yyyy}${mm}${dd}`
+    const formattedTime = `${hh}${min}00`
+
+    const convertedDateTime = `${formattedDate}T${formattedTime}%2F${formattedDate}T${formattedTime}&ctz`
+
+    return convertedDateTime
+  }
+
+  const convertTextToURL = (text) => {
+    const encodedText = encodeURIComponent(text)
+    return encodedText
+  }
+
+  const title = convertTextToURL(
+    `Appointment with ${info.name} for ${info.purpose}}`
+  )
+  const desc = convertTextToURL(
+    `Appointment with ${info.name} for ${info.purpose}} at ${info.time} on ${info.date}, ${info.day}`
+  )
+  const dates = convertDateToURL(`${info.date} ${info.time}`)
+
   const templateParams = {
     name: info.name, // name of the appointment requester
     email: info.email, // email of the appointment requester
@@ -8,6 +34,7 @@ export const sendEmail = (info) => {
     date: info.date, // date of the appointment
     day: info.day, // week day of the appointment
     time: info.time, // time of the appointment
+    link: `https://calendar.google.com/calendar/r/eventedit?text=${title}&details=${desc}&dates=${dates}&ctz=Asia%2FDhaka`,
   }
 
   emailjs
