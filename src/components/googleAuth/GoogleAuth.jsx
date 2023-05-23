b78db06593159e5bd2ec5537de6401a37bcee799
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2023-05-23 23:37:16 +0600

    fixed admin text shows briefly at appointment page visit

--- src/components/googleAuth/GoogleAuth.jsx
+++ src/components/googleAuth/GoogleAuth.jsx
@@ -9,10 +9,13 @@ import { addGoogleUser } from '../../configs/googleUser'
 export const GoogleAuth = () => {
   // Fetch admin list
   const [adminList, setAdminList] = useState([])
+  const [adminListFetched, setAdminListFetched] = useState(false) // New state to track if admin list data has been fetched
+
   useEffect(() => {
     const fetchAdmin = async () => {
       const data = await fetchData('admin')
       setAdminList(data)
+      setAdminListFetched(true) // Set the adminListFetched state to true once data is fetched
     }
 
     fetchAdmin()
@@ -60,27 +63,28 @@ export const GoogleAuth = () => {
   return (
     <>
       <div className="text-center">
-        {!user && (
-          <div>
-            {/* If admin list doesn't have any data */}
+        {!user &&
+          adminListFetched && ( // Add adminListFetched check here
             <div>
-              <p className="text-lg mb-4">
-                {adminList.length === 0
-                  ? 'There are no admins in the database. Sign to register as admin.'
-                  : 'Please Sign In to Make an Appointment'}
-              </p>
+              {/* If admin list doesn't have any data */}
+              <div>
+                <p className="text-lg mb-4">
+                  {adminList.length === 0
+                    ? 'There are no admins in the database. Sign in to register as an admin.'
+                    : 'Please Sign In to Make an Appointment'}
+                </p>
+              </div>
+              <div className="flex justify-center">
+                <button
+                  onClick={signInWithGoogle}
+                  className="rounded-xl border-2 border-blue-400 bg-blue-100 hover:bg-blue-300 text-slate-700 font-semibold py-2 px-4 flex items-center text-3xl"
+                >
+                  <FcGoogle className="mr-2" />
+                  Sign In With Google
+                </button>
+              </div>
             </div>
-            <div className="flex justify-center">
-              <button
-                onClick={signInWithGoogle}
-                className="rounded-xl border-2 border-blue-400 bg-blue-100 hover:bg-blue-300 text-slate-700 font-semibold py-2 px-4 flex items-center text-3xl"
-              >
-                <FcGoogle className="mr-2" />
-                Sign In With Google
-              </button>
-            </div>
-          </div>
-        )}
+          )}
       </div>
     </>
   )
