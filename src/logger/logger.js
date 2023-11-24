107dc4c4858ba43b6ac1f2df03fdb9d0f4e25128
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2023-11-25 00:48:23 +0600

    added logger

---
+++ src/logger/logger.js
@@ -0,0 +1,51 @@
+const logTypes = {
+  log: {
+    color: 'white',
+  },
+  info: {
+    color: 'cyan',
+  },
+  warn: {
+    color: 'orange',
+  },
+  error: {
+    color: 'red',
+  },
+  debug: {
+    color: 'yellow',
+  },
+  success: {
+    color: 'green',
+  },
+}
+
+const logger = {}
+
+Object.keys(logTypes).forEach((type) => {
+  logger[type] = (message, messageColor = 'inherit', dateColor = 'skyblue') => {
+    if (
+      process.env.NODE_ENV !== 'production' &&
+      process.env.NEXT_PUBLIC_ENV !== 'production'
+    ) {
+      const timestamp = new Date().toISOString()
+      if (typeof message === 'string') {
+        console.log(
+          `%c${timestamp} | %c${type.toUpperCase()}\t: %c${message}`,
+          `color: ${dateColor}`,
+          `color: ${logTypes[type].color}`,
+          `color: ${messageColor}`
+        )
+      } else {
+        console.log(
+          `%c${timestamp} | %c${type.toUpperCase()}\t: %c${'[Object]'}`,
+          `color: ${dateColor}`,
+          `color: ${logTypes[type].color}`,
+          `color: ${messageColor}`
+        )
+        console.log(message)
+      }
+    }
+  }
+})
+
+export default logger
