ca2d94c9fb161f13c46c957a0b9a526e96fc3433
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2023-12-04 22:56:15 +0600

    finalize gallery page

--- src/pages/gallery/Gallery.jsx
+++ src/pages/gallery/Gallery.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Gallery as G } from '@/components'
+import useDocumentTitle from '@/assets/js/useDocumentTitle'
+import Info from '@/configs/data'
+
 import './gallery.css'
 
 const Gallery = () => {
-  return <div>Gallery</div>
+  useDocumentTitle('Gallery')
+
+  return (
+    <div className="flex w-full md:w-1/2 lg:w-1/3 justify-center items-center">
+      {Info.gallery.map((item, index) => (
+        <G key={index} props={item}></G>
+      ))}
+    </div>
+  )
 }
 
 export default Gallery
