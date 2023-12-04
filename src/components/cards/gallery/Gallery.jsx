fe239acd3fa8c51a23fba813c59888750e3d7fa0
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2023-12-04 22:55:43 +0600

    added Gallery component

---
+++ src/components/cards/gallery/Gallery.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Card, CardHeader, CardBody } from '@nextui-org/react'
+
+const Gallery = (props) => {
+  const { title, category, provider, place, date, guest, image } = props.props
+
+  return (
+    <Card className="flex gap-2 py-1 my-6 w-full">
+      <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
+        <p className="text-tiny uppercase font-bold">{category}</p>
+        <div>
+          <small className="text-default-600 p-1 bg-gray-200/90 rounded">
+            {date}
+          </small>{' '}
+          <small className="text-default-500">{place}</small>
+        </div>
+        <h4 className="font-bold text-large">{title}</h4>
+        <small className="text-default-500 font-semibold">{provider}</small>
+      </CardHeader>
+      <CardBody className="w-full py-2 items-center justify-center">
+        <img
+          alt="Card background"
+          className="rounded-xl w-full h-full object-cover"
+          src={image}
+        />
+      </CardBody>
+    </Card>
+  )
+}
+
+export default Gallery
