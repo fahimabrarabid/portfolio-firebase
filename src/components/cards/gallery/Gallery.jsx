c67f6c9cb636d8d33d52e3308cc77da2882e4bb3
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2023-12-05 00:39:40 +0600

    make all text justify in gallery card

--- src/components/cards/gallery/Gallery.jsx
+++ src/components/cards/gallery/Gallery.jsx
@@ -7,15 +7,17 @@ const Gallery = (props) => {
   return (
     <Card className="flex h-full py-1 mt-6 w-full md:w-4/12 lg:w-1/4">
       <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
-        <p className="text-tiny uppercase font-bold">{category}</p>
+        <p className="text-tiny uppercase font-bold text-justify">{category}</p>
         <div>
-          <small className="text-default-600 p-1 bg-gray-200/90 rounded">
+          <small className="text-default-600 p-1 bg-gray-200/90 rounded text-justify">
             {date}
           </small>{' '}
           <small className="text-default-500">{place}</small>
         </div>
-        <h4 className="font-bold text-large">{title}</h4>
-        <small className="text-default-500 font-semibold">{provider}</small>
+        <h4 className="font-bold text-large text-justify">{title}</h4>
+        <small className="text-default-500 font-semibold text-justify">
+          {provider}
+        </small>
       </CardHeader>
       <CardBody className="w-full py-2 items-center justify-center">
         <img
