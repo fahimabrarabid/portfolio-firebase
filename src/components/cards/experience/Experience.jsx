e677dc8e4947316cf334782a47f9bdb34babe46b
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2023-12-14 09:58:22 +0600

    added support for html in exp title

--- src/components/cards/experience/Experience.jsx
+++ src/components/cards/experience/Experience.jsx
@@ -9,7 +9,7 @@ const Experience = (Info) => {
 
       {Info.experience.map((exp, index) => (
         <div key={index} className="job">
-          <h3>{exp.company}</h3>
+          <h3>{parse(exp.company)}</h3>
           <div className="flex justify-between">
             {exp.position ? <h4>{exp.position}</h4> : ''}
             {exp.duration ? <h5 className="text-right">{exp.duration}</h5> : ''}
